cc39643e272f4d06de68f3c54de7478cb50e638a
kangxi11 <rkang2009@gmail.com>
2019-10-16 15:06:41 -0600

    initial data and .py

--- mp1.py
+++ mp1.py
@@ -1,1 +1,17 @@
-nothing yet
+import sqlite3
+import time
+
+connection = None
+c = None # cursor
+
+def main():
+    database_name = input('Database name: ')
+    
+    conn = sqlite3.connect(database_name)
+    c = conn.cursor()
+
+    
+
+    print('success')
+
+main()
