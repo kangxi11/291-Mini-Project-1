e899993020cd31b1a094bd7409e8ace29bc85b01
Phiangdao Aubin <phiangda@cs.ualberta.ca>
2019-10-21 18:22:19 -0600

    added agent menu

--- agent.py
+++ agent.py
@@ -1,6 +1,10 @@
 # for all the functionalities of the agent
 import sqlite3
 
-def agent_screen(user):
-    print('You have successfully logged')
-    print(user)
+def clear_screen():
+    print('----------------------------------------------------------------------------------------------')
+    print('\n\n\n\n')
+
+def agent_menu(user):
+    print('Hello Agent', user[4])
+    
