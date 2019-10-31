f0969d2cba830f9ba512d2de7856ef8234ad5aac
Daniel <Dannyhan8698@gmail.com>
2019-10-31 13:19:06 -0600

    Updates

--- agent.py
+++ agent.py
@@ -495,12 +495,9 @@ def a5(c, connection):
                 break
 
     #Retrieve fine amount from ticket issued
-    c.execute("SELECT amount FROM payments WHERE tno=?;", (tno,))
-    temp = c.fetchall()
-    fine_total = 0
-    for x in temp:
-        fine_total = fine_total + int(x[0])
-    
+    c.execute("SELECT SUM(amount) FROM payments WHERE tno=?;", (tno,))
+    fine_total = c.fetchall()[0]
+
     c.execute("SELECT fine FROM tickets WHERE tno = ?;",(tno,))
     fine_remaining = c.fetchone()[0] - fine_total
 
@@ -539,20 +536,6 @@ def a5(c, connection):
         garbage = input('Press Enter to Continue')
     else:
         garbage = input('Press Enter to Continue')
-
-        
-
-    #If the full amount is not paid off, user can choose to pay in lump payments
-    #while ((fine - amount) != 0):
-    #    try:
-    #        tno = input("Enter ticket number: ")
-    #        amount = input("Enter amount: ")
-     #         pdate = datetime.date.today()
-     #       payment = (tno, pdate, amount)
-     #       c.execute("INSERT INTO payments (tno, pdate, amount) VALUES (?,?,?);", payment)
-    #
-     #   except AssertionError as error:
-    #        print(error)
 
 def a6(c, connection):
     print('Please provide the following information to get a driver abstract: ')
